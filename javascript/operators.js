faac8fa45095967df86258e4689e5e3f552ac699
swathi <swathi.ponnam638@gmail.com>
2024-10-15 16:08:49 +0530

    responsive website

---
+++ javascript/operators.js
@@ -0,0 +1,13 @@
+// terinary operator
+
+// let marks = 75;
+ 
+// (marks<35) ? console.log("failed") : 
+// (marks>=35 && marks <=60 ) ? console.log( "3rd class" ) : 
+// (marks >60 && marks <=75) ? console.log("2nd class") :
+// (marks >75 && marks <=85) ? console.log( "1st class" ) :
+// (marks > 85 && marks <=100) ? console.log("Distintion") : console.log("wrong input")
+
+
+let arr =[20,40,30,50,22,56]
+
