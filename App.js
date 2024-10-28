a0074e67fa638cb63dad18125801deb4c3d41369
swathiiponnam <163500248+swathiiponnam@users.noreply.github.com>
2024-10-28 19:15:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import Body from './component/Body';
+import Counter from './component/Counter';
+import Nav from './component/Nav';
+import TodoList from './component/TodoList';
+
+function App() {
+  return (
+    <div>
+      <Nav/>
+      {/* <Body/> */}
+      {/* <Counter/> */}
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
